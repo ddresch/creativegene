7f7a0e172da315f369e14fe407830c7880a72269
Dirk Dresch <dirk.dresch@gmail.com>
2022-08-07 22:58:49 +0200

    Add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import type { NextPage } from 'next'
 import { useAccount } from 'wagmi'
 import CreateGene from '../components/CreateGene'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   const { isConnected } = useAccount()
@@ -12,6 +13,8 @@ const Home: NextPage = () => {
       <div className="container">
         <div style={{ flex: '1 1 auto' }}>
           <div style={{ padding: '24px 24px 24px 0' }}>
+            
+            <Image src="/logo.png" alt="creativegene Logo" width={128} height={128} />
             
             <h1>creativegene</h1>
             
